022b3d9f2408a50e4036b1fdaa1b9d574c7cada1
Silas Green <scgreen160@gmail.com>
2025-05-14 13:06:08 -0400

    closer, need to fix userReponse()

--- scraper.py
+++ scraper.py
@@ -1,53 +1,65 @@
 from PIL import Image
 from tkinter import Tk, filedialog
 import pytesseract
-import os
-import emoji
-import time
+import os, sys, emoji, time
 
 ##FUNCTIONS
 
 #gives CommandLine response a human touch
-def printSlow(text, delay=0.80):
+#iterates through text and slowly types
+#gaurantees a new line after text is done
+def printSlow(text, delay=.05, end="\n", flush=True):
   for char in text:
-    print(char, end="", flush=True)
+    print(char, end="", flush=flush)
     time.sleep(delay)
-def userinputResponse(self, user_input):
+  print(end=end, flush=flush)
+#recursive function giving user option to overwrite .py file
+#while also error handling invalid inputs
+def userinputResponse():
+  while True:
+    user_input = printSlow(input("You already have an image_text.py, do you want to overwrite it? Y/N: "))
     if user_input.lower() == "y":
       with open("image_text.py", "w") as image_text:
         image_text.write(text)
-      print(emoji.emojize(":check_mark_button: Overwritten existing image_text.py file..."))
-    #delete current image_text.py file
-    #then create new one
+      printSlow(emoji.emojize(":check_mark_button: Overwritten existing image_text.py file..."))
+      sys.exit()
     elif user_input.lower() == "n":
       programShutdown()
     else:
       printSlow(f"Sorry, but {user_input} isn't a valid input")
-    self.userinputResponse(user_input)
+    userinputResponse()
 #throws some attitude when it has to exit
-def programShutdown():
-  printSlow("Uh...", end="", flush=True)
-  time.sleep(1)
-  print("Okay...", end="", flush=True)
-  time.sleep(1)
-  printSlow("shutting down... delete the current image_text.py and try again!")
-  exit()
+def programShutdown(self):
+    printSlow("Uh...", flush=True, end=" ")
+    time.sleep(1)
+    printSlow("Okay...", flush=True, end=" ")
+    time.sleep(1)
+    printSlow("shutting down... delete the current image_text.py and try again!")
+    sys.exit()
+## END of FUNCTIONS
 
 ## MAIN-RUN
+
+#error handles if image_scraper is not installed
 try:
-    pytesseract.get_tesseract_version()
+  pytesseract.get_tesseract_version()
 except pytesseract.TesseractNotFoundError:
-    print(emoji.emojize(":cross_mark_button: Tesseract is not in your PATH or not installed."))
-    exit()
+  print(emoji.emojize(":cross_mark_button: Tesseract is not in your PATH or not installed."))
+  sys.exit()
 
 root = Tk()
 root.withdraw() #closes TK window
 printSlow("Select your text image...")
-#opens local window for user to select image 
+#opens local window for user to select image
+
 image_path = filedialog.askopenfilename(
   title="Choose A Screenshot of Python Code (nothing more...nothing less)",
   filetypes=[("Image files", "*.png *.jpg *.jpeg *.bmp *.tiff")]
   )
+if not image_path: 
+  printSlow(emoji.emojize(":cross_mark_button: No file selected, shutting down..."))
+  sys.exit()
+
 #checks to see if the image_path exists
 #if True, confirm and display PATH
 #if False, display error and exit
@@ -55,7 +67,7 @@ try:
     image = Image.open(image_path)
 except FileNotFoundError:
     printSlow(f"Image path '{image_path}' not found...")
-    exit()
+    sys.exit()
 printSlow(f"Got it!: ", end="")
 printSlow(image_path)
 
@@ -69,8 +81,7 @@ try:
   image_text.close()
   #OVERWITE-block... if file already exists, gives option to overwrite
 except FileExistsError:
-  user_input = input("You already have an image_text.py, do you want to overwrite it? Y/N: ")
-  userinputResponse(user_input)
+  userinputResponse()
  
     
    
